8c8f35dddd2d4d0e55f6b6b146b3f2bfd6af3166
HommiePeter <peter.scholtens@home.nl>
2021-03-28 00:11:56 +0100

    0.39

--- src/ToonConnection.ts
+++ src/ToonConnection.ts
@@ -98,8 +98,8 @@ export default class ToonConnection {
 
       const fetch = require('node-fetch');
 
-      this.log.info('GET URL' url)
-      
+      this.log.info('GET URL' url);
+
       const response =  await fetch( url, {
           method: "GET",
           headers: this.getHeader
