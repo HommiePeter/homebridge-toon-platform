89b75444bd475ba4c8cf9897799107df72183f8c
HommiePeter <peter.scholtens@home.nl>
2021-04-03 09:19:36 +0200

    0.46

---
+++ src/Old/ToonConnection.ts
@@ -0,0 +1,214 @@
+
+import {
+    API,
+    APIEvent,
+    CharacteristicEventTypes,
+    CharacteristicSetCallback,
+    CharacteristicValue,
+    DynamicPlatformPlugin,
+    HAP,
+    Logging,
+    PlatformAccessory,
+    PlatformAccessoryEvent,
+    PlatformConfig,
+  } from "homebridge";
+
+import {
+  API_URL,
+  BASE_URL,
+  ThermostatInfo,
+  Token,
+  ToonAgreement,
+  ToonStatus
+} from "./ToonAPI-Definitions";
+
+export class ToonConnection {
+    private agreement?: ToonAgreement; 
+    private toonStatus?: ToonStatus;
+    private agreementIndex: number;
+     
+    private token?: string;
+    private resultjson!: string;
+
+    constructor(
+      private config: PlatformConfig,
+      private log: Logging,
+    /*  private toonstatus: ToonStatus,
+      private agreement: ToonAgreement */
+      private onUpdate: (toonStatus: ToonStatus) => void 
+    ) {
+      this.token = config.apiToken;
+  
+      // Index selecting the agreement, if a user has multiple agreements (due to moving, etc.).
+      this.agreementIndex = this.config.agreementIndex
+        ? this.config.agreementIndex
+        : 0;
+  
+      this.initialize().then(() => {
+            setInterval(this.getToonStatus, 10000);
+            this.log("interval is set based on toon status")
+        });
+    }
+  
+    private async initialize() {
+      this.agreement = await this.getAgreementData();
+    }
+  
+    private getHeader() {
+      return {
+        Authorization: `Bearer ${this.token}`,
+        "content-type": "application/json",
+        "cache-control": "no-cache"
+      };
+    }
+  
+    private async toonPUTRequest(url: string, body: any) {
+      if (this.token === undefined) {
+        throw Error("PUT not authorized");
+      }
+      const fetch = require('node-fetch');
+
+      try {
+        const response = await fetch (url, { 
+          'method': 'put', 
+          'body': JSON.stringify(body), 
+          'headers': this.getHeader
+        })
+        return await response.json();
+      }
+      catch(err) {
+        this.log.info(`Oeps PUT went wrong ${err}`) // Maybe present some error/failure UI to the user here
+      };
+      
+    }
+  
+    private async toonGETRequest(url: string) {
+      if (this.token === undefined) {
+        throw Error("GET not authorized");
+      }
+      const fetch = require('node-fetch');
+      
+      try {
+        const response = await fetch( url, {
+            'method': 'GET',
+            'headers': this.getHeader()
+        })
+        return await response.json();
+      }
+      catch(err) {
+        this.log.info(`Oeps GET went wrong ${err}`) // Maybe present some error/failure UI to the user here
+      };
+    } 
+  
+    private async getAgreementData() {
+      this.log.info("Getting agreement...");
+  
+      let agreements: ToonAgreement[] = await this.toonGETRequest(
+        `${API_URL}agreements`
+      );
+
+      if (this.agreementIndex < agreements.length) {
+        this.log.info(`Currently selected agreementIndex: ${this.agreementIndex}`);
+        const { street, houseNumber, postalCode, city, heatingType } = agreements[this.agreementIndex];
+        this.log.info(`Agreement selected for: ${street} ${houseNumber} ${postalCode} ${city} ${heatingType}`);
+        return agreements[this.agreementIndex];
+
+      } else {
+        for (const agreementIndex in agreements) {
+          const {street,houseNumber,postalCode,city,heatingType } = agreements[agreementIndex];
+
+          this.log.info(
+            `agreementIndex: [${agreementIndex}]: ${street} ${houseNumber} ${postalCode} ${city} ${heatingType}`
+          );
+        }
+  
+        throw new Error(
+          "Incorrect agreementIndex selected, is your config valid?"
+        );
+      }
+    }
+    private getToonStatus = async () => {
+        if (!this.agreement) {
+          throw Error("Requested status but there is no agreement.");
+        }
+    
+        let toonStatus: ToonStatus = await this.toonGETRequest(
+          `${API_URL}${this.agreement.agreementId}/status`
+        );
+    
+        if (toonStatus.thermostatInfo) { 
+          this.toonStatus = toonStatus;
+          this.onUpdate(this.toonStatus);
+        } 
+      }; 
+      private async setToonTemperature(temperature: number) {
+        if (!this.agreement) {
+          throw Error("Setting temperature but there is no agreement.");
+        }
+    
+        if (!this.toonStatus) {
+          throw Error("Setting temperature but there is no status information.");
+        }
+    
+        this.log(`Setting Toon Temperature to ${temperature / 100}`);
+    
+        let currentThermostatInfo: ThermostatInfo = await this.toonGETRequest(
+          `${API_URL}${this.agreement.agreementId}/thermostat`
+        );
+    
+        const payload = {
+          ...currentThermostatInfo,
+          currentSetpoint: temperature,
+          activeState: -1,
+          programState: 2
+        };
+    
+        const newThermostatInfo = await this.toonPUTRequest(
+          `${API_URL}${this.agreement.agreementId}/thermostat`,
+          payload
+        );
+    
+        this.log(`Successfully set Toon Temperature to ${temperature / 100}`);
+    
+        this.toonStatus.thermostatInfo = newThermostatInfo;
+        this.onUpdate(this.toonStatus);
+      }
+    
+      public async setTemperature(temperature: number) {
+        const destination_temperature = Math.round(
+          (Math.round(temperature * 2) / 2) * 100
+        );
+    
+        await this.setToonTemperature(destination_temperature);
+      }
+    
+      public getDisplayCommonName() {
+        return this.agreement ? this.agreement.displayCommonName : "-";
+      }
+    
+      public getHardwareVersion() {
+        return this.agreement ? this.agreement.displayHardwareVersion : "-";
+      }
+    
+      public getSoftwareVersion() {
+        return this.agreement ? this.agreement.displaySoftwareVersion : "-";
+      }
+    
+      public getBurnerInfo() {
+        return this.toonStatus
+          ? this.toonStatus.thermostatInfo.burnerInfo
+          : undefined;
+      }
+    
+      public getCurrentTemperature() {
+        return this.toonStatus
+          ? this.toonStatus.thermostatInfo.currentDisplayTemp / 100
+          : undefined;
+      }
+    
+      public getCurrentSetpoint() {
+        return this.toonStatus
+          ? this.toonStatus.thermostatInfo.currentSetpoint / 100
+          : undefined;
+      }
+  }
---
+++ src/Old/dynamic-platform.ts
@@ -0,0 +1,186 @@
+import http, {IncomingMessage, Server, ServerResponse} from "http";
+import {
+  API,
+  APIEvent,
+  CharacteristicEventTypes,
+  CharacteristicSetCallback,
+  CharacteristicValue,
+  DynamicPlatformPlugin,
+  HAP,
+  Logging,
+  PlatformAccessory,
+  PlatformAccessoryEvent,
+  PlatformConfig,
+} from "homebridge";
+
+
+import { ToonThermostat } from "./ToonThermostat"
+import { ToonStatus, ThermostatInfo, ToonAgreement } from "./ToonAPI-Definitions"
+
+const PLUGIN_NAME = "homebridge-toon-platform";
+const PLATFORM_NAME = "Toon-Platform";
+
+/*
+ * IMPORTANT NOTICE
+ *
+ * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
+ * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
+ * and will disappear once the code is compiled to Javascript.
+ * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
+ * You will notice that the file does not contain a `... = require("homebridge");` statement anywhere in the code.
+ *
+ * The contents of the above import statement MUST ONLY be used for type annotation or accessing things like CONST ENUMS,
+ * which is a special case as they get replaced by the actual value and do not remain as a reference in the compiled code.
+ * Meaning normal enums are bad, const enums can be used.
+ *
+ * You MUST NOT import anything else which remains as a reference in the code, as this will result in
+ * a `... = require("homebridge");` to be compiled into the final Javascript code.
+ * This typically leads to unexpected behavior at runtime, as in many cases it won't be able to find the module
+ * or will import another instance of homebridge causing collisions.
+ *
+ * To mitigate this the {@link API | Homebridge API} exposes the whole suite of HAP-NodeJS inside the `hap` property
+ * of the api object, which can be acquired for example in the initializer function. This reference can be stored
+ * like this for example and used to access all exported variables and classes from HAP-NodeJS.
+ */
+let hap: HAP;
+let Accessory: typeof PlatformAccessory;
+var Service: any, Characteristic: any
+
+export = (api: API) => {
+  hap = api.hap;
+  Accessory = api.platformAccessory;
+  Service = api.hap.Service;
+  Characteristic = api.hap.Characteristic
+  api.registerPlatform(PLATFORM_NAME, ToonPlatform);
+};
+
+class ToonPlatform implements DynamicPlatformPlugin {
+  Thermostat! : ToonThermostat;
+
+  private readonly log: Logging;
+  private readonly config: PlatformConfig;
+  private readonly api: API;
+  
+  private requestServer?: Server;
+  private readonly accessories: PlatformAccessory[] = [];
+ 
+  private toonstatus!: ToonStatus;
+  private agreement! :ToonAgreement;
+
+  constructor(log: Logging, config: PlatformConfig, api: API) {
+    this.log = log;
+    
+    // probably parse config or something here
+    log.info("Toon-Platform: Reading config");
+    this.config = config;
+
+  /*  this.connection = new ToonConnection(this.config, this.log, this.toonstatus, this.agreement);
+
+  /*  this.connection = new ToonConnection(this.config, this.log ); */
+
+    this.api = api;
+    log.info("Example platform finished initializing!");
+
+    /*
+     * When this event is fired, homebridge restored all cached accessories from disk and did call their respective
+     * `configureAccessory` method for all of them. Dynamic Platform plugins should only register new accessories
+     * after this event was fired, in order to ensure they weren't added to homebridge already.
+     * This event can also be used to start discovery of new accessories.
+     */
+    api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
+      log.info("Example platform 'didFinishLaunching'");
+
+      // The idea of this plugin is that we open a http service which exposes api calls to add or remove accessories
+      this.log ("Toon-Platform : Start Add Thermostat");
+      this.addThermostat();
+    });
+  }
+
+  /*
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to setup event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory): void {
+    this.log("Configuring accessory %s", accessory.displayName);
+
+    accessory.on(PlatformAccessoryEvent.IDENTIFY, () => {
+      this.log("%s identified!", accessory.displayName);
+    });
+
+    accessory.getService(hap.Service.Thermostat)!
+      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+        this.log.info("%s Light was set to: " + value);
+        callback();
+      });
+
+    this.accessories.push(accessory);
+  }
+
+  // --------------------------- CUSTOM METHODS ---------------------------
+
+  addThermostat() {
+    if (this.Thermostat !== undefined) {
+      this.log.info("addThermostat: Thermostat already existing");
+      return;
+    }
+    this.log.info("addThermostat: Adding accessory Thermostat");
+
+    const accessory = new Accessory( "Toon Thermostaat", hap.uuid.generate("Toon Thermostaat") );
+   
+    this.log.info("addThermostat: Added accessory Thermostat");
+
+    this.log.info("addThermostat: Setup new Toon Thermostat");
+
+    this.Thermostat = new ToonThermostat(accessory, this.config, this.log);
+
+    this.log.info("addThermostat: new Toon Thermostat was Setup");
+
+    this.log.info("addThermostat: registerPlaformAccessories")
+
+    this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+  }
+  /* Original
+  addAccessory(name: string) {
+    this.log.info("Adding new accessory with name %s", name);
+
+    // uuid must be generated from a unique but not changing data source, name should not be used in the most cases. But works in this specific example.
+    const uuid = hap.uuid.generate(name);
+    const accessory = new Accessory(name, uuid);
+
+    accessory.addService(hap.Service.Lightbulb, "Test Light");
+
+    this.configureAccessory(accessory); // abusing the configureAccessory here
+
+    this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+  } */
+
+  removeAccessories() {
+    // we don't have any special identifiers, we just remove all our accessories
+
+    this.log.info("Removing all accessories");
+
+    this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, this.accessories);
+    this.accessories.splice(0, this.accessories.length); // clear out the array
+  }
+
+  /*
+  createHttpService() {
+    this.requestServer = http.createServer(this.handleRequest.bind(this));
+    this.requestServer.listen(18081, () => this.log.info("Http server listening on 18081..."));
+  } */
+
+  /*
+  private handleRequest(request: IncomingMessage, response: ServerResponse) {
+    if (request.url === "/add") {
+      this.addAccessory(new Date().toISOString());
+    } else if (request.url === "/remove") {
+      this.removeAccessories();
+    } 
+
+    response.writeHead(204); // 204 No content
+    response.end();
+  } */
+
+  // ----------------------------------------------------------------------
+
+}
