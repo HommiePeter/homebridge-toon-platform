cf4a9efd6e0dead26bf2618e2edef3fb203bc69c
HommiePeter <peter.scholtens@home.nl>
2021-04-17 18:02:37 +0200

    0.85

--- src/ToonWallPlug.ts
+++ src/ToonWallPlug.ts
@@ -62,9 +62,14 @@ export class ToonWallPlug {
         .onGet(this.handleOutletInUseGet.bind(this))
         .onSet(this.handleOutletInUseSet.bind(this));
 
-        this.service.getCharacteristic(this.platform.Characteristic.On)
-        .onGet(this.handleOnGet.bind(this))
-        .onSet(this.handleOnSet.bind(this));
+    //    this.service.getCharacteristic(this.platform.Characteristic.On)
+    //    .onGet(this.handleOnGet.bind(this))
+    //    .onSet(this.handleOnSet.bind(this));
+
+        this.service
+                .getCharacteristic(this.platform.Characteristic.On)
+                .on("set", this.handleOnSet)
+                .on("get", this.handleOnGet);
     }
 
     handleOutletInUseSet(value: CharacteristicValue) {
