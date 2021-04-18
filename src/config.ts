2ab39dfc0cd50bdfd66883a42b492ea25d10214c
HommiePeter <peter.scholtens@home.nl>
2021-04-18 22:21:17 +0200

    0.85

--- src/config.ts
+++ src/config.ts
@@ -7,4 +7,20 @@ export default interface ToonConfig {
   
     // API token from https://api.toon.eu/toonapi-accesstoken?tenant_id=eneco&client_id=<consumer_key>
     apiToken: string;
+    
+    // Switch to indicate if available connected Hue lights should be imported into the Toon-platform
+    switch_hue: boolean;
+   
+    // Switch to indicate if available connected smoke sensors should be imported into the Toon-platform
+    switch_smoke:boolean;
+    
+    // Switch to indicate if available connected wall plugs should be imported into the Toon-platform
+    switch_wallplug: boolean;
+   
+    },
+
+
+
+
+
   }
