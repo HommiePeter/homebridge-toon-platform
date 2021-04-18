8e42572d012db086a45ad1d8e8047de36198ecda
HommiePeter <peter.scholtens@home.nl>
2021-04-18 11:23:35 +0200

    0.85

--- src/ToonThermostat.ts
+++ src/ToonThermostat.ts
@@ -39,7 +39,7 @@ export class ToonThermostat {
             .setCharacteristic(this.platform.Characteristic.FirmwareRevision, this.toon.connection.getSoftwareVersion())                
             .setCharacteristic(this.platform.Characteristic.HardwareRevision, this.toon.connection.getHardwareVersion());
 
-        this.thermostatService = this.accessory.getService(this.platform.Service.Thermostat) || this.accessory.addService(this.platform.Service.Thermostat);
+       // this.thermostatService = this.accessory.getService(this.platform.Service.Thermostat) || this.accessory.addService(this.platform.Service.Thermostat);
         
     /*    this.thermostatService
             .getCharacteristic(this.platform.Characteristic.TargetHeatingCoolingState)
@@ -103,31 +103,33 @@ export class ToonThermostat {
                 .on("get", this.getTemperatureDisplayUnits);
         } else {  
             this.log.info("ToonThermostat Const:Thermostat Update")
+            const thermostatService = this.accessory.getService(this.platform.Service.Thermostat);
             const { thermostatInfo } = this.toon.connection.toonstatus;
+            if (thermostatService) {
+                this.thermostatService= thermostatService
             
-            this.thermostatService = this.accessory.getService(this.platform.Service.Thermostat);
+                this.thermostatService.updateCharacteristic(
+                    this.platform.Characteristic.CurrentTemperature,
+                    thermostatInfo.currentDisplayTemp / 100
+                );
 
-            this.thermostatService.updateCharacteristic(
-                this.platform.Characteristic.CurrentTemperature,
-                thermostatInfo.currentDisplayTemp / 100
-            );
-
-            this.thermostatService.updateCharacteristic(
-                this.platform.Characteristic.TargetTemperature,
-                thermostatInfo.currentSetpoint / 100
-            );
+                this.thermostatService.updateCharacteristic(
+                    this.platform.Characteristic.TargetTemperature,
+                    thermostatInfo.currentSetpoint / 100
+                );
               
-            var heatingCoolingState;
-            if (thermostatInfo.burnerInfo === "1") {
-                heatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.HEAT;
-            } else {
-                heatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.OFF;
-            }
+                var heatingCoolingState;
+                if (thermostatInfo.burnerInfo === "1") {
+                    heatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.HEAT;
+                } else {
+                    heatingCoolingState = this.platform.Characteristic.CurrentHeatingCoolingState.OFF;
+                }
   
-            this.thermostatService.updateCharacteristic(
-                this.platform.Characteristic.CurrentHeatingCoolingState,
-                heatingCoolingState
-            );
+                this.thermostatService.updateCharacteristic(
+                    this.platform.Characteristic.CurrentHeatingCoolingState,
+                    heatingCoolingState
+                );
+            }
         } 
     }
     
