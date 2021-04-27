9fccdfbb013ab9863046c8197562be0be50b508b
HommiePeter <peter.scholtens@home.nl>
2021-04-27 10:53:59 +0200

    0.9

--- src/CustomCharacteristic.js
+++ src/CustomCharacteristic.js
@@ -1,5 +1,5 @@
 
-var Service, Characteristic;
+var PowerService, PowerCharacteristic, Characteristic;
 var inherits = require('util').inherits;
 
 var CustomCharacteristic = {};
@@ -7,23 +7,23 @@ var CustomCharacteristic = {};
 //Initialize
 module.exports = function (homebridge) {
 	PowerService = homebridge.Service;
-	Characteristic = homebridge.Characteristic;
+	PowerCharacteristic = homebridge.Characteristic;
 
     var CustomCharacteristic = {};
 
 	CustomCharacteristic.CurrentPowerConsumption = function () {
-		Characteristic.call(this, 'Consumption', 'E863F10D-079E-48FF-8F27-9C2605A29F52');
+		PowerCharacteristic.call(this, 'Consumption', 'E863F10D-079E-48FF-8F27-9C2605A29F52');
 		this.setProps({
-			format: Characteristic.Formats.UINT32,
+			format: PowerCharacteristic.Formats.UINT32,
 			unit: "Watt",
 			maxValue: 100000,
 			minValue: 0,
 			minStep: 1,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+			perms: [PowerCharacteristic.Perms.READ, PowerCharacteristic.Perms.NOTIFY]
 		});
 		this.value = this.getDefaultValue();
 	};
-	inherits(CustomCharacteristic.CurrentPowerConsumption, Characteristic);
+	inherits(CustomCharacteristic.CurrentPowerConsumption, PowerCharacteristic);
 
 	CustomCharacteristic.DailyPowerConsumption = function () {
         Characteristic.call(this, 'Energy', 'E863F10C-079E-48FF-8F27-9C2605A29F52');
