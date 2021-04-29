60ed0c17b8726986120aa04002650037c343d0cc
HommiePeter <peter.scholtens@home.nl>
2021-04-29 22:54:28 +0200

    0.9

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,12 @@ var Service, Characteristic, UUIDGen, CustomCharacteristic;
  * This method registers the platform with Homebridge
  */
 
- export = (api: API) => {
-//  export default function(homebridge: any) {
-    Service = api.hap.Service;
-	  Characteristic = api.hap.Characteristic;
-	  UUIDGen = api.hap.uuid;
-    //CustomCharacteristic = require('./CustomCharacteristic.js')(api);
+// export = (api: API) => {
+ export default function(homebridge: any) {
+    Service = homebridge.hap.Service;
+	  Characteristic = homebridge.hap.Characteristic;
+	  UUIDGen = homebridge.hap.uuid;
+    CustomCharacteristic = require('./CustomCharacteristic.js')(homebridge);
   
-    api.registerPlatform(PLATFORM_NAME, ToonHomebridgePlatform);
+    homebridge.registerPlatform(PLATFORM_NAME, ToonHomebridgePlatform);
   };
